3ed5c4826ce83daf49131eb55b15c39d779bfb57
et1902 <43827504+et1902@users.noreply.github.com>
2020-04-03 19:47:14 +0200

    fixed merged

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  //templateUrl: './app.component.html',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-
-  template: `
-    <h1>{{title}}</h1>
-    <h2>This guy is gay: {{name}}</h2>
-    `
   
 })
 export class AppComponent {
   title = 'goosegame';
-  name = 'Elias';
+
 }
