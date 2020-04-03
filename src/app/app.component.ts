c9169054e8420db0dab0f9a7341afd07ae9a6119
et1902 <43827504+et1902@users.noreply.github.com>
2020-04-03 20:21:29 +0200

    add buttons to start and join game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,18 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  
 })
 export class AppComponent {
   title = 'goosegame';
+
+  onClickNewGame()
+  {
+    console.log("Start new game")
+  }
+
+  onClickJoinGame()
+  {
+    console.log("Join game")
+  }
 
 }
