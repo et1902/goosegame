f0634d2b99eeadd515af28782a3a081439e863fc
et1902 <43827504+et1902@users.noreply.github.com>
2020-04-03 18:14:32 +0200

    init

---
+++ src/app/game.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+    selector: 'app-game',
+    templateUrl: './game.component.html',
+    styleUrls: ['./game.component.css']
+})
+export class GameComponent {
+    gameId;
+
+    constructor( private route: ActivatedRoute )
+    { }
+
+    ngOnInit()
+    {
+        this.route.paramMap.subscribe(  params => { this.gameId = params.get('gameId'); } );
+     }
+    
+}
