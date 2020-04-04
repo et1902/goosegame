c94f7f525b7ad6ebfea8f09a539f033ead7cc4be
Joshua Rosenberger <rosenbergerjoshua@web.de>
2020-04-04 20:05:57 +0200

    added PlayerBuilder test for next id

--- src/app/helper/player-builder.spec.ts
+++ src/app/helper/player-builder.spec.ts
@@ -4,4 +4,12 @@ describe('PlayerBuilder', () => {
   it('should create an instance', () => {
     expect(new PlayerBuilder()).toBeTruthy();
   });
+  
+  it('correct next id', () => {
+    var builder: PlayerBuilder = new PlayerBuilder()
+    expect(builder.newPlayer("depp").getPlayerId()).toBe(1);
+    expect(builder.newPlayer("depp").getPlayerId()).toBe(2);
+    expect(builder.newPlayer("depp").getPlayerId()).toBe(3);
+  });
+
 });
